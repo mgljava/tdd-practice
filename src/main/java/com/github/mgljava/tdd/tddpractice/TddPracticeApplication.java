d58e40dfd6eb113d0694573dcdb1c0346e35985d
Minggl <guangliang1120@gmail.com>
2019-06-04 15:14:08 +0800

    [TECH][TDD] init project

---
+++ src/main/java/com/github/mgljava/tdd/tddpractice/TddPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.github.mgljava.tdd.tddpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddPracticeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TddPracticeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/github/mgljava/tdd/tddpractice/TddPracticeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.mgljava.tdd.tddpractice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TddPracticeApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
